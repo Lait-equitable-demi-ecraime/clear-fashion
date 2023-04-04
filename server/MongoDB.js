da9b5066431ea8ec973c7cdd657c696bba443aaa
Lait-equitable-demi-ecraime <mrninja185@gmail.com>
2023-04-04 23:52:41 +0200

    pw update pc1

--- server/MongoDB.js
+++ server/MongoDB.js
@@ -1,29 +1,14 @@
-const {MongoClient} = require('mongodb');
-const MONGODB_URI = 'mongodb+srv://charles:charles1@clearfashioncluster.ed8xkyr.mongodb.net/?retryWrites=true&w=majority';
-const MONGODB_DB_NAME = 'clearfashion';
+const {MongoClient, ObjectId} = require('mongodb');
+var MONGODB_URI = 'mongodb+srv://charles:charles1@clearfashioncluster.ed8xkyr.mongodb.net/?retryWrites=true&w=majority';
+const MONGODB_DB_NAME = 'clearfashion_db';
+const MONGODB_COLLECTION = 'clearfashion_collection';
+const fs = require('fs');
+var collection = null;
 
-let client = null;
-let database = null;
 
-/**
- * Get db connection
- * @type {MongoClient}
- */
-const getDB = module.exports.getDB = async () => {
-    try {
-    if (database) {
-        console.log('💽  Already Connected');
-        return database;
-    }
-    `mongodb+srv://charles:charles1@clearfashioncluster.ed8xkyr.mongodb.net/?retryWrites=true&w=majority`
-    client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
-    database = client.db(MONGODB_DB_NAME);
-
-    console.log('💽  Connected');
-
-    return database;
-    } catch (error) {
-    console.error('🚨 MongoClient.connect...', error);
-    return null;
-    }
-};
+async function connectMongoDb(){
+    console.log('Connecting to MongoDB ...');
+    const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+    const db =  client.db(MONGODB_DB_NAME);
+    collection = db.collection(MONGODB_COLLECTION);
+}
