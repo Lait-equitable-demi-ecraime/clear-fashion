2ada98bb42666f1a9c28b3ef08c7499939b0ba13
Lait-equitable-demi-ecraime <mrninja185@gmail.com>
2023-04-05 01:21:54 +0200

    pw update pc1

--- server/MongoDB.js
+++ server/MongoDB.js
@@ -1,77 +1,75 @@
-const {MongoClient, ObjectId} = require('mongodb');
-var MONGODB_URI = 'mongodb+srv://charles:charles1@clear-fashion.vvfza2l.mongodb.net/?retryWrites=true&w=majority';
-const MONGODB_DB_NAME = 'clear-fashion';
-const MONGODB_COLLECTION = 'clear-fashion';
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const fs = require('fs');
-var collection = null;
 
-async function connectMongoDb(){
-    console.log('Connecting to MongoDB ...');
-    const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
-    const db =  client.db(MONGODB_DB_NAME);
-    collection = db.collection(MONGODB_COLLECTION);
+const MONGODB_URI = 'mongodb+srv://charles:charles1@clear-fashion.vvfza2l.mongodb.net/?retryWrites=true&w=majority';
+const MONGODB_DB_NAME = 'clear-fashion';
+const MONGODB_COLLECTION = 'products';
 
-connectMongoDb()
-
+async function ConnectMongoDb() {
+    console.log('Connecting to MongoDB...');
+    const client = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true });
+    const db = client.db(MONGODB_DB_NAME);
+    const collection = db.collection(MONGODB_COLLECTION);
+    return { client, collection };
 }
-async function insertProductsMongoDB() {
-    await connectMongoDb();
+
+async function InsertProducts() {
+    const { client, collection } = await ConnectMongoDb();
     try {
-        console.log('Inserting products to the MongoDB database');
-    
-        const products_to_insert_1 = JSON.parse(fs.readFileSync('products_file_1.json'));
-        const products_to_insert_2 = JSON.parse(fs.readFileSync('products_file_2.json'));
-        const products_to_insert_3 = JSON.parse(fs.readFileSync('products_file_3.json'));
-    
-        const collection = db.collection('products');
-    
-        const inserted_products_1 = await collection.insertMany(products_to_insert_1);
-        console.log(inserted_products_1);
-    
-        const inserted_products_2 = await collection.insertMany(products_to_insert_2);
-        console.log(inserted_products_2);
-    
-        const inserted_products_3 = await collection.insertMany(products_to_insert_3);
-        console.log(inserted_products_3);
-    
+        console.log('Inserting products to MongoDB');
+
+        const data_dedicatedbrand = JSON.parse(fs.readFileSync('data_dedicatedbrand.json'));
+        const data_montlimart = JSON.parse(fs.readFileSync('data_montlimart.json'));
+        const data_circlesportswear = JSON.parse(fs.readFileSync('data_circlesportswear.json'));
+
+        const inserted_data_dedicatedbrand = await collection.insertMany(data_dedicatedbrand);
+        console.log(inserted_data_dedicatedbrand);
+
+        const inserted_data_montlimart = await collection.insertMany(data_montlimart);
+        console.log(inserted_data_montlimart);
+
+        const inserted_data_circlesportswear = await collection.insertMany(data_circlesportswear);
+        console.log(inserted_data_circlesportswear);
+
         console.log('Products inserted successfully.');
     } catch (err) {
         console.error(err);
-        } finally {
-            client.close();
-            }
+    } finally {
+        client.close();
+    }
 }
 
+InsertProducts();
 
-insertProductsMongoDB();
 
-async function fetchProductsByBrand() {
-    await connectMongoDb();
+async function ByBrand() {
+    await ConnectMongoDb();
     console.log('Fetching products from same brand...');
     const brand = 'Dedicated';
     const products = await collection.find({"brand" : brand}).toArray();
     return products;
 }
 
-async function fetchProductByID() {
-    await connectMongoDb();
-    console.log('Fetching product by ID...');
-    const productID = "640f3707b8266552d9bf5504";
-    const products = await collection.find({ "_id": ObjectId(productID) }).toArray();
+async function LessThanPrice() {
+    await ConnectMongoDb();
+    console.log('Fetching products less than a selected price...');
+    const price = 10;
+    const products = await collection.find({"price": {$lt:price}}).toArray();
     console.log(products);
     process.exit(0);
 }
 
-async function fetchProductsSortedByPrice() {
-    await connectMongoDb();
+async function fSortedByPrice() {
+    await ConnectMongoDb();
     console.log('Fetching products sorted by price...');
-    const products = await collection.find().sort({price: 1}).toArray();
+    const products = await collection.find().sort({price: 34}).toArray();
     console.log(products);
     process.exit(0);
 }
 
-async function fetchProductsSortedByDate() {
-    await connectMongoDb();
+async function fSortedByDate() {
+    await ConnectMongoDb();
     console.log('Fetching products sorted by date...');
     const products = await collection.find().sort({date: -1}).toArray();
     console.log(products);
