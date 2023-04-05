037d196b1dc7acb25665c4551f5611884d69a1fe
Lait-equitable-demi-ecraime <mrninja185@gmail.com>
2023-04-05 21:55:35 +0200

    update_api

--- server/api.js
+++ server/api.js
@@ -1,8 +1,11 @@
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
+const MongoClient = require('mongodb').MongoClient;
 
 const PORT = 8092;
+const MONGODB_URI = 'mongodb+srv://charles:charles1@clear-fashion.vvfza2l.mongodb.net/?retryWrites=true&w=majority'; // replace this with your MongoDB Atlas URI
+const DB_NAME = 'clear-fashion'; // replace this with your database name
 
 const app = express();
 
@@ -15,12 +18,32 @@ app.use(helmet());
 app.options('*', cors());
 
 app.get('/', (request, response) => {
-  response.send({'ack': true});
+  // connect to the database
+  MongoClient.connect(MONGODB_URI, function(err, client) {
+    if (err) {
+      console.error('Failed to connect to the database:', err);
+      return response.status(500).send('Failed to connect to the database');
+    }
+
+    const db = client.db(DB_NAME);
+    const productsCollection = db.collection('products');
+
+    // retrieve the products from the database
+    productsCollection.find({}).toArray(function(err, products) {
+      if (err) {
+        console.error('Failed to retrieve products:', err);
+        return response.status(500).send('Failed to retrieve products');
+      }
+
+      // send the products as the response
+      response.send(products);
+
+      // close the database connection
+      client.close();
+    });
+  });
 });
 
-
-
 app.listen(process.env.PORT || 3000, () => console.log(`Server started on port ${process.env.PORT || 3000}`));
-
 
 console.log(`📡 Running on port ${PORT}`);
